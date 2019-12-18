2ac25f937e78cb527a79fb326954f3a05becc979
Sebastian Park <SebPark03@gmail.com>
2019-12-17 22:42:27 -0500

    Update Camera.dart
    Add encoding and special printing that will print out all characters. Reverse decoded each image to check if encoder worked; success.

--- dex_mobile/lib/Camera.dart
+++ dex_mobile/lib/Camera.dart
@@ -43,10 +43,8 @@ class TakePictureScreenState extends State<SnapScreen> {
     // TODO: implement build
 
     return Scaffold(
-      appBar:
-      AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.red,
-
       ),
       body: FutureBuilder<void>(
         future: initCamController,
@@ -85,7 +83,6 @@ class TakePictureScreenState extends State<SnapScreen> {
   }
 }
 
-
 class DisplayPictureScreen extends StatelessWidget {
   /*static*/ final String imagePath;
 
@@ -95,33 +92,44 @@ class DisplayPictureScreen extends StatelessWidget {
 
   const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
 
+  void printWrapped(String text) {
+    final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
+    pattern.allMatches(text).forEach((match) => print(match.group(0)));
+  }
+
+  String encodeImg() {
+    File imageFile = new File(this.imagePath);
+    List<int> imageBytes = imageFile.readAsBytesSync();
+    String base64Image = base64.encode(imageBytes);
+    printWrapped("{" + base64Image + "} Not shortened");
+    return base64Image.length.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
+    String encodedImg = encodeImg();
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          title: Text('Photo', style: TextStyle(color: Colors.black),
-
+          title: Text(
+            'Photo',
+            style: TextStyle(color: Colors.black),
           ),
           iconTheme: IconThemeData(
             color: Colors.black, //change your color here
           ),
           elevation: 0,
-
         ),
         // The image is stored as a file on the device. Use the `Image.file`
         // constructor with the given path to display the image.
-        body:
-        Container(
+        body: Container(
             child: Align(
-              child: Image.file(File(imagePath)),
-              alignment: Alignment.center,
-            )
-        )
-    );
+//              child: Image.file(File(imagePath)),
+          child: Text(encodedImg),
+          alignment: Alignment.center,
+        )));
   }
 }
